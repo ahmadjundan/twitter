0684f69c0e893f9758f20c668468c824ba3b9c97
ahmadjundan <ahjundan160@gmail.com>
2023-07-11 03:01:37 +0700

    Image Picker & Carousel Slider

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,11 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void showSnackbar(BuildContext context, String message) {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+void showSnackbar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
+    content: Text(content),
   ));
 }
 
 String getNameFromEmail (String email) {
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imageFiles = await picker.pickMultiImage();
+  if (imageFiles.isNotEmpty) {
+    for (final image in imageFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
